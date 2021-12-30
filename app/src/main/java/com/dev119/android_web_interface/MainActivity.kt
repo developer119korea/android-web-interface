717b00305709ecf2b8e601d92e084b263c746e27
Youngwan Cho <developer119korea@gmail.com>
2021-12-30 17:51:31 +0900

    웹뷰 생성

--- app/src/main/java/com/dev119/android_web_interface/MainActivity.kt
+++ app/src/main/java/com/dev119/android_web_interface/MainActivity.kt
@@ -2,10 +2,41 @@ package com.dev119.android_web_interface
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebChromeClient
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val webView = findViewById<WebView>(R.id.webview);
+        initializeWebView(webView);
+        var url = "https://google.com";
+        webView.loadUrl(url);
+    }
+
+    fun initializeWebView(webView: WebView) {
+        webView.apply {
+            webViewClient = WebViewClient();
+            webChromeClient = WebChromeClient()
+            settings.javaScriptEnabled = true
+            settings.javaScriptCanOpenWindowsAutomatically = false
+            settings.setSupportMultipleWindows(false)
+            settings.loadsImagesAutomatically = true
+            settings.useWideViewPort = true
+            settings.loadWithOverviewMode = true
+            settings.setSupportZoom(true)
+            settings.builtInZoomControls = false
+            settings.displayZoomControls = false
+            settings.cacheMode = WebSettings.LOAD_NO_CACHE
+            settings.domStorageEnabled = true
+            settings.allowContentAccess
+            settings.userAgentString = "app"
+            settings.defaultTextEncodingName = "UTF-8"
+            settings.databaseEnabled = true
+        }
     }
 }
